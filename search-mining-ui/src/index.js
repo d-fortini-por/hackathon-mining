94f2687bb16e447927f037c35e393c3491ca1a05
akabakibi <akabakibi@bol.com>
2020-05-02 14:30:23 +0200

    Add Search box with dummy data

--- search-mining-ui/src/index.js
+++ search-mining-ui/src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/search/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Search from "./components/search";
+import 'semantic-ui-css/semantic.min.css'
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Search />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
